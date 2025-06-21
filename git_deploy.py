0470b91edea72758b86fc550e55c9d62baee6aa9
vulnbox <vulnbox@cyberchallenge>
2025-06-21 16:42:39 +0200

    Minor fixes

--- git_deploy.py
+++ git_deploy.py
@@ -1,50 +1,72 @@
 import os
 import subprocess
 from pathlib import Path
+from dotenv import load_dotenv
+from os import getenv
+from contextlib import suppress
 import yaml
 
-LOCAL_BASE_PATH = "root"
-
+load_dotenv()
+LOCAL_BASE_PATH = getenv("LOCAL_BASE_PATH")
 
 def create_ignore(folder_path):
-    docker_compose = Path(os.path.join(folder_path, "docker-compose.yml"))
-    if docker_compose.is_file():
-        print(f"Trovato docker-compose.yaml in {folder_path}")
+    docker_compose_yml = Path(os.path.join(folder_path, "docker-compose.yml"))
+    docker_compose_yaml = Path(os.path.join(folder_path, "docker-compose.yaml"))
+    docker_compose = docker_compose_yml if docker_compose_yml.is_file() else docker_compose_yaml
 
-        git_ignore = Path(os.path.join(folder_path, ".gitignore"))
-        with open(git_ignore, 'a') as f_ignore:
-            with open(docker_compose, 'r') as f:
-                compose_yaml = yaml.safe_load(f)
+    if not docker_compose.is_file(): return
+    print(f"Trovato {docker_compose}")
 
-            volumi = compose_yaml.get('volumes', {})
-            if volumi:
-                for nome_volume in volumi:
-                    f_ignore.write(f"\n{nome_volume.split(':')[0]}")
+    with open(docker_compose, 'r') as f:
+        content  = f.read()
+        compose_yaml = yaml.safe_load(content.strip())
 
-            servizi = compose_yaml.get('services', {})
-            for _, config_servizio in servizi.items():
-                volumi_servizio = config_servizio.get('volumes', [])
-                if volumi_servizio:
-                    for nome_volume in volumi_servizio:
-                        f_ignore.write(f"\n{nome_volume.split(':')[0]}")
+    git_ignore = Path(os.path.join(folder_path, ".gitignore"))
+    ignore_dirs = set()
+
+    with suppress(Exception):
+        servizi = compose_yaml.get('services', {})
+
+        for config_servizio in servizi.values():
+            volumi_servizio = config_servizio.get('volumes', [])
+            if not volumi_servizio: continue
+            
+            for nome_volume in volumi_servizio:
+                bind_mnt: str = nome_volume.split(':')[0]
+                if bind_mnt.startswith("./") or bind_mnt.startswith("/"):
+                    bind_mnt = bind_mnt if bind_mnt.endswith("/") else bind_mnt + "/"
+                    ignore_dirs.add("\n" + bind_mnt)
+    
+    with open(git_ignore, 'a') as f_ignore:
+        f_ignore.writelines(ignore_dirs)
 
 
-def initialize_and_push(folder_path):  # def initialize_and_push(folder_path, github_repo_url):
+def initialize(folder_path):
     try:
-        # subprocess.run(["git", "config", "--global http.postBuffer 1048576000"], cwd=folder_path, check=True)
-        subprocess.run(["git", "config", "--global", "user.name", "git_deploy.py"], cwd=folder_path, check=True)
-        subprocess.run(["git", "config", "--global", "user.email", "git_deploy"], cwd=folder_path, check=True)
+        subprocess.run(["git", "config", "--global", "user.name", "vulnbox"], cwd=folder_path, check=True)
+        subprocess.run(["git", "config", "--global", "user.email", "vulnbox@cyberchallenge"], cwd=folder_path, check=True)
         subprocess.run(["git", "init"], cwd=folder_path, check=True)
         subprocess.run(["git", "add", "."], cwd=folder_path, check=True)
         subprocess.run(["git", "commit", "-m", "Initial commit"], cwd=folder_path, check=True)
-
+        with suppress(subprocess.CalledProcessError):
+            subprocess.run(["git", "branch", "backup"], cwd=folder_path, check=True)
         print(f"[↑] Creato repository {folder_path}")
     except subprocess.CalledProcessError as e:
         print(f"[!] Errore in {folder_path}: {e}")
 
+def main():
+    print(LOCAL_BASE_PATH)
+    dirs = filter(
+        os.path.isdir,
+        map(
+            lambda d: os.path.join(LOCAL_BASE_PATH, d),
+            os.listdir(LOCAL_BASE_PATH)
+        )
+    )
 
-for item in os.listdir(LOCAL_BASE_PATH):
-    item_path = os.path.join(LOCAL_BASE_PATH, item)
-    create_ignore(item_path)
-    print(f"📁 Trovata cartella: {item}")
-    initialize_and_push(item_path)
+    for dir_path in dirs:
+        create_ignore(dir_path)
+        print(f"📁 Trovata cartella: {dir_path}")
+        initialize(dir_path)
+
+if __name__ == "__main__": main()
